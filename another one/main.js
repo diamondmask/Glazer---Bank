80ec985c37525e5624ce5b2466e8e49dcd1a9f49
diamondmask <diamondrichard654@gmail.com>
2023-01-27 20:36:00 +0100

    Initial commit

---
+++ another one/main.js
@@ -0,0 +1,22 @@
+console.log(window)
+const btnEl = document.querySelector(".btn");
+const closeEl = document.querySelector(".close-icon");
+const trailerContainer = document.querySelector(".trailer-container");
+const videoEl = document.querySelector("video");
+
+
+btnEl.addEventListener("click", ()=>{
+    trailerContainer.classList.remove("active");
+}); 
+
+closeEl.addEventListener("click", ()=>{
+    trailerContainer.classList.add("active");
+    videoEl.pause();
+    videoEl.currentTime = 0;
+});
+
+
+
+window.onscroll = calcscrollValue;
+window.load = calcscrollValue;
+
